047167b156de12f781ee09c3358afd6659fa32f7
kritisrivas <kritisrivastava.28@gmail.com>
2024-09-11 13:42:39 +1000

    port change for backend deployment

--- backend/app.js
+++ backend/app.js
@@ -38,4 +38,4 @@ app.use((error, req, res, next) => {
   
 // Connect to MongoDB and start server
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iimpyqt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
-.then(()=>app.listen(5000)).catch();
+.then(()=>app.listen(process.env.PORT || 5000)).catch();
